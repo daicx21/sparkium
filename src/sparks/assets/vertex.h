3d7583d991d91bf8515d16117d4e85e3daaf88db
unknown <ma-yh21@mails.tsinghua.edu.cn>
2023-12-30 15:37:31 +0800

    tangent fix bug

--- src/sparks/assets/vertex.h
+++ src/sparks/assets/vertex.h
@@ -5,7 +5,7 @@ namespace sparks {
 struct Vertex {
   glm::vec3 position{};
   glm::vec3 normal{0.0f, 0.0f, 1.0f};
-  glm::vec3 tangent{1.0f, 0.0f, 0.0f};
+  glm::vec3 tangent{0.0f, 0.0f, 0.0f};
   glm::vec2 tex_coord{};
 
   Vertex() = default;
